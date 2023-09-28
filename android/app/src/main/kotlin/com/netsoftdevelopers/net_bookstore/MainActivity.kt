392fb221d2d00bdc90ddaed0999d6227d8a09ed5
IKENNA COLLINS <obettaikenna19@gmail.com>
2023-09-28 13:33:32 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/netsoftdevelopers/net_bookstore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.netsoftdevelopers.net_bookstore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/service/payment_paypal.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_paypal_checkout/flutter_paypal_checkout.dart';
+
+//undefined
+
+Future<void> payPalPayments(
+  BuildContext context,
+  double amount,
+  double discount,
+) async {
+  PaypalCheckout(
+    sandboxMode: true,
+    clientId:
+        "ATtJ2gpE39yBQjGb9bW9IFVaGhGJwTko2LZQ-bZLFr3FhV35DHvez5ENz5joI3Aap7bABqDGVfjQJ6u",
+    secretKey: "SECRET_KEY",
+    returnURL: "RETURN_URL",
+    cancelURL: "CANCEL_URL",
+    transactions: [
+      {
+        "amount": {
+          "total": '$amount',
+          "currency": "USD",
+          "details": {
+            "subtotal": '$amount',
+            "shipping": '0',
+            "shipping_discount": discount
+          }
+        },
+        "description": "The payment transaction description.",
+        "item_list": {
+          "items": [
+            {"name": "Apple", "quantity": 4, "price": '5', "currency": "USD"},
+            {
+              "name": "Pineapple",
+              "quantity": 5,
+              "price": '10',
+              "currency": "USD"
+            }
+          ],
+          // shipping address is Optional
+          "shipping_address": {
+            "recipient_name": "Raman Singh",
+            "line1": "Delhi",
+            "line2": "",
+            "city": "Delhi",
+            "country_code": "IN",
+            "postal_code": "11001",
+            "phone": "+00000000",
+            "state": "Texas"
+          },
+        }
+      }
+    ],
+    note: "PAYMENT_NOTE",
+    onSuccess: (Map params) async {
+      print("onSuccess: $params");
+    },
+    onError: (error) {
+      print("onError: $error");
+      Navigator.pop(context);
+    },
+    onCancel: () {
+      print('cancelled:');
+    },
+  );
+}
